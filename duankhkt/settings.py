4d40a30aea48c846a5cc1f51dadffce47b655c03
Enomus123 <ttungduong2000@gmail.com>
2025-10-25 22:28:07 +0700

    fix template path

--- duankhkt/settings.py
+++ duankhkt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'duankhkt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'app' / 'templates'],
+        'DIRS': [BASE_DIR / 'app' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
