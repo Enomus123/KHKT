412c3f2df9bc1ffa696ee89a3562e61d2a2706dc
Enomus123 <ttungduong2000@gmail.com>
2025-10-25 21:12:59 +0700

    Fix template path case issue

--- duankhkt/settings.py
+++ duankhkt/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-afgy2&&02o#eq=cerjtpv91r)-wk-(b@nr$rf(0fq_)&1cmow*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = False
 ALLOWED_HOSTS = ['khkt-s1n9.onrender.com', 'localhost', '127.0.0.1']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
