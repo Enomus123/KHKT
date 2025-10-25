7f23b14b9323b324b0142cbd7c549f4f8b02bdd3
Enomus123 <ttungduong2000@gmail.com>
2025-10-25 21:44:17 +0700

    enable debug mode for testing

--- duankhkt/settings.py
+++ duankhkt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'duankhkt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'duankhkt.wsgi.application'
 
